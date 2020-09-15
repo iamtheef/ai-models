18c2517786c82f65a39189419fc1e5ac37390a40
iamtheef <eftihis.riise@gmail.com>
2020-09-15 22:59:45 +0300

    logostic regression

---
+++ logistic-regression-model/logistic-regression-model.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+# importing data
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+# splitting the columns
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# splitting to test set and train set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+
+
+
+
+
+
