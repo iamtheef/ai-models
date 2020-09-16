798f9788add6adabb61455af99cc579bde14bfa0
iamtheef <eftihis.riise@gmail.com>
2020-09-16 22:35:32 +0300

    logistic regression

--- logistic-regression-model/logistic-regression-model.py
+++ logistic-regression-model/logistic-regression-model.py
@@ -24,6 +24,9 @@ X_test = sc.transform(X_test)
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train, y_train)
 
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
 
 
 
