8e2ea6c89aadd5b96443bca1e1febcbe7756e3c2
iamtheef <eftihis.riise@gmail.com>
2020-10-11 18:44:20 +0300

    evaluations of classification models

---
+++ EVALUATIONS/Classification/decision_tree_classification.py
@@ -0,0 +1,33 @@
+# Decision Tree Classification
+
+# Importing the libraries
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# Training the Decision Tree Classification model on the Training set
+classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+classifier.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
---
+++ EVALUATIONS/Classification/support_vector_machine.py
@@ -0,0 +1,32 @@
+# Support Vector Machine (SVM)
+
+# Importing the libraries
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the SVM model on the Training set
+classifier = SVC(kernel='linear', random_state=0)
+classifier.fit(X_train, y_train)
+
+
+# Making the Confusion Matrix
+y_pred = classifier.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
