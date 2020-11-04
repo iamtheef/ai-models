d99a955f7803092a209b039c48ba9900fbcdaaee
iamtheef <eftihis.riise@gmail.com>
2020-11-04 19:20:53 +0200

    model can classify cats/dogs

--- Deep Learning/Convolutional Neural Networks/CNN.py
+++ Deep Learning/Convolutional Neural Networks/CNN.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
+from keras_preprocessing import image
+import numpy as np
 
 tf.__version__
 
@@ -53,5 +55,18 @@ cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # training the model
-cnn.fit(X=training_set, validation_data=test_set, epochs=25)
+# cnn.fit(x=training_set, validation_data=test_set, epochs=25)
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_4.jpg', target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+        prediction = 'dog'
+else:
+        prediction = 'cat'
+
+print(prediction)
+
 
